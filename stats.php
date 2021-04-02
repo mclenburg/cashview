d58f6b4fefa1744d44b1adf12bf256f388d7b4d9
mclenburg <>
2021-04-02 09:45:35 +0200

    added script-tags ;P

--- stats.php
+++ stats.php
@@ -295,9 +295,11 @@
                           </div>
                       </div>
     </div>
+    <script>
     $(document).ready(function () {
     $('#KatTable').DataTable();
     $('.dataTables_length').addClass('bs-select');
     });
+    </script>
 </body>
 </html>
