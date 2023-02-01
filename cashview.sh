2b7e7007664992d505091f0eb539d4175a0745c7
mclenburg <binimmerich@gmail.com>
2023-02-01 07:44:32 +0100

    added manId to shell-script

--- cashview.sh
+++ cashview.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mysql -h 192.168.5.103 -u cashview -p cashview --password=cash123 <<EOF
-insert into transaktionen(Wert, KtoID, katID, Datum) (select Wert, KtoID, katID, now() from laufendes where mod(MONTH(now()), modulo)=0);
+insert into transaktionen(Wert, KtoID, katID, Datum, manId) (select Wert, KtoID, katID, now(), manId from laufendes where mod(MONTH(now()), modulo)=0);
 commit;
 exit
 EOF
